9c52404f246360623c4763994988876fce8b6261
Payton Kentch <frankrlystan@gmail.com>
2023-07-13 04:36:32 -0600

    initial commit

---
+++ json-demo/itunes_json.js
@@ -0,0 +1,9 @@
+import mountainSongs from './mountain.json' assert { type: "json" };
+
+//NOTE: It may help to view/transform the JSON through a formatter
+
+//1. Log an array of the keys of the main object in the JSON file
+
+//2. Get the title of the first track in the data
+
+//3. Get all the artists present in the data
