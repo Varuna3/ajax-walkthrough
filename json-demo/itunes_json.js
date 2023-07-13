0aa2c099e9d2a54461f2078c4a6cd4c1a9a61956
Payton Kentch <frankrlystan@gmail.com>
2023-07-13 04:54:36 -0600

    finished demo exercises

--- json-demo/itunes_json.js
+++ json-demo/itunes_json.js
@@ -1,9 +1,18 @@
-import mountainSongs from './mountain.json' assert { type: "json" };
+import mountainSongs from './mountain.json' assert { type: 'json' }
 
 //NOTE: It may help to view/transform the JSON through a formatter
 
 //1. Log an array of the keys of the main object in the JSON file
+const arr = []
+for (const key in mountainSongs) {
+  arr.push(key)
+}
+console.log(arr)
 
 //2. Get the title of the first track in the data
+console.log(mountainSongs.results[0].trackName)
 
 //3. Get all the artists present in the data
+const artists = []
+mountainSongs.results.forEach(e => artists.push(e.artistName))
+console.log(artists)
